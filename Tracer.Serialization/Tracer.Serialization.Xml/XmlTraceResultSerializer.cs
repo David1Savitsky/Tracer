408771d521ae8ae1ee59f7dd903ddeb810aa4e41
David1Savitsky <omgprogress24@gmail.com>
2022-09-19 15:29:00 +0300

    Add xml serializer

---
+++ Tracer.Serialization/Tracer.Serialization.Xml/XmlTraceResultSerializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+using Tracer.Core.DataModels;
+using Tracer.Serialization.Abstractions;
+using Tracer.Serialization.Xml.DataModels;
+
+namespace Tracer.Serialization.Xml;
+
+public class XmlTraceResultSerializer : ITraceResultSerializer
+{
+    public void Serialize(TraceResult traceResult, Stream to)
+    {
+        var serializer = new XmlSerializer(typeof(TraceResultInfo));
+        var model = new TraceResultInfo(traceResult);
+        serializer.Serialize(to, model);
+    }
+}
