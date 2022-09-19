a1dc510c7500bbc51276d9bdb2e3e3b3267c24dd
David1Savitsky <omgprogress24@gmail.com>
2022-09-19 15:28:19 +0300

    Add yaml serialization logic

---
+++ Tracer.Serialization/Tracer.Serialization.Yaml/DataModels/TraceResultInfo.cs
@@ -0,0 +1,18 @@
+﻿using Tracer.Core.DataModels;
+
+namespace Tracer.Serialization.Yaml.DataModels;
+
+public class TraceResultInfo
+{
+    public List<ThreadInfo> Threads { get; set; }
+
+    public TraceResultInfo(TraceResult result)
+    {
+        Threads = new(result.Threads.Count);
+        foreach (var thread in result.Threads)
+        {
+            var model = new ThreadInfo(thread.Value);
+            Threads.Add(model);
+        }
+    }
+}
